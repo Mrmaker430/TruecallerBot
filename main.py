058a9b66c51a2a6da2943c57e0b10872dea64990
Mr maker <167637714+Mrmaker430@users.noreply.github.com>
2025-05-30 14:45:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ load_dotenv()  # Loads values from .env
 API_ID = int(os.getenv("API_ID",""))
 API_HASH = os.getenv("API_HASH","")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID",""))
+OWNER_ID = int(os.getenv("OWNER_ID","1773034985"))
 API_URL = os.getenv("API_URL")
 
 # MongoDB Connection
